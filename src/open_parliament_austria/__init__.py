bee88533044365f84c47a8b3e0c888ce747bab2f
jan <152862650+j-haacker@users.noreply.github.com>
2025-10-16 21:33:27 +0200

    Add geschichtsseiten download

--- src/open_parliament_austria/__init__.py
+++ src/open_parliament_austria/__init__.py
@@ -2,23 +2,16 @@
 
 __all__ = []
 
+from datetime import datetime
 import json
+import numpy as np
 from pathlib import Path
 from pypdf import PdfReader
 import requests
-from typing import Literal
+import sqlite3
+from typing import Any, Literal
 
 lib_data = Path.home() / ".open-parliament-austria"
-
-
-def _extract_txt_from_pdf(pdf_file: Path | str):
-    reader = PdfReader(pdf_file)
-    pages = reader.pages
-    return "\n".join([p.extract_text() for p in pages])
-
-
-def _prepend_url(path: str) -> str:
-    return "https://www.parlament.gv.at" + path
 
 
 def _download_collection_metadata(
@@ -51,3 +44,49 @@ def _download_file(url: str, target: Path):
         for chunk in res.iter_content(chunk_size=4096):
             if chunk:
                 f.write(chunk)
+
+
+def _extract_txt_from_pdf(pdf_file: Path | str):
+    reader = PdfReader(pdf_file)
+    pages = reader.pages
+    return "\n".join([p.extract_text() for p in pages])
+
+
+def _get_colnames(con: sqlite3.Connection, tbl: str) -> list[str]:
+    return [
+        row[0]
+        for row in con.execute(
+            f"SELECT name FROM pragma_table_info('{tbl}')"
+        ).fetchall()
+    ]
+
+
+def _get_colname_by_type(con: sqlite3.Connection, tbl: str, _type: str) -> list[str]:
+    return [
+        row[0]
+        for row in con.execute(
+            f"SELECT name FROM pragma_table_info('{tbl}') WHERE type = '{_type.upper()}'"
+        ).fetchall()
+    ]
+
+
+def _prepend_url(path: str) -> str:
+    return "https://www.parlament.gv.at" + path
+
+
+def _sqlite3_type(x: Any) -> str:
+    match x:
+        case None:
+            return "NULL"
+        case int() | np.integer() | bool() | np.bool():
+            return "INTEGER"
+        case float() | np.floating():
+            return "REAL"
+        case datetime():
+            return "DATETIME"
+        case str():
+            return "TEXT"
+        case set() | list() | tuple() | dict():
+            return "BLOB"
+    # anything can be stored as blob, however for hygiene raise
+    raise Exception(f"Couldn't determine sqlite3 type for {x}, class {x.__class__}.")
--- src/open_parliament_austria/antraege_etc.py
+++ src/open_parliament_austria/antraege_etc.py
@@ -12,20 +12,24 @@ Choices may need to change in future to support more datasets.
 __all__ = []
 
 from ast import literal_eval
+from bs4 import BeautifulSoup
 import numpy as np
 from open_parliament_austria import (
     _download_collection_metadata,
     _download_file,
     _extract_txt_from_pdf,
+    _get_colnames,
+    _get_colname_by_type,
     lib_data,
     _prepend_url,
+    _sqlite3_type,
 )
 import pandas as pd
 from pathlib import Path
 import pickle
 import requests
 import sqlite3
-from typing import Literal
+from typing import Any, Literal
 
 index_col = ["GP_CODE", "ITYP", "INR"]
 raw_data = lib_data / "data" / "raw"
@@ -87,19 +91,167 @@ def _build_global_metadataframe_from_json(
 def _download_document(df_row: pd.Series):
     url = _prepend_url(df_row.HIS_URL) + "?json=True"
     _dict = requests.get(url).json()
+    print(_dict)
     if "documents" in _dict["content"]:
         docs = _dict["content"]["documents"]
+    print(docs)
     if "docs" not in locals() or len(docs) == 0:
         raise Exception(f"No docs for {df_row.name}.")
-    elif len(docs) > 1:
+    elif len(docs) > 2:
+        # See what docs are included and implement handling
         raise Exception(f"Not implmented. Docs:\n{repr(docs)}")
+    elif len(docs) > 1:
+        # prefer digital version
+        if any(["(elektr. übermittelte Version)" in doc["title"] for doc in docs]):
+            docs = next(
+                doc["documents"]
+                for doc in docs
+                if "(elektr. übermittelte Version)" in doc["title"]
+            )
+        # otherwise select original
+        elif any(["(gescanntes Original)" in doc["title"] for doc in docs]):
+            docs = next(
+                doc["documents"]
+                for doc in docs
+                if "(gescanntes Original)" in doc["title"]
+            )
+        else:
+            docs = docs[0]["documents"]
+    if any(["HTML" in d["type"] for d in docs]):
+        link = next(d["link"] for d in docs if "HTML" in d["type"])
     else:
-        doc = docs[0]["documents"][0]
-        if doc["type"] != "PDF":
+        doc = docs[0]
+        if docs["type"] != "PDF":
             raise Exception(f"Not implemented for type {doc['type']}.")
         link = doc["link"]
-        path = Path(raw_data, *list(map(str, df_row.name)))
-        _download_file(_prepend_url(link), path / link.split("/")[-1])
+    path = Path(raw_data, *list(map(str, df_row.name)))
+    _download_file(_prepend_url(link), path / link.split("/")[-1])
+
+
+def pd_read_sql(
+    con: sqlite3.Connection,
+    tablename: str,
+    columns: list[str] | None = None,
+    index: tuple[str, str, int] | None = None,
+) -> pd.DataFrame:
+    datetime_cols = _get_colname_by_type(con, tablename, "datetime")
+    pickled_cols = _get_colname_by_type(con, tablename, "blob")
+    kwargs = dict(
+        con=con,
+        index_col=index_col,
+        parse_dates=datetime_cols,
+    )
+    if index is None and columns is None:
+        df = pd.read_sql(tablename, **kwargs)
+    elif index is None:
+        df = pd.read_sql(
+            f"SELECT {'*' if columns is None else ', '.join(columns)} FROM {tablename}",
+            **kwargs,
+        )
+    elif len(index) == 3 and isinstance(index[0], str):  # then its a single index
+        df = pd.read_sql(
+            f"SELECT {'*' if columns is None else ', '.join(columns)} "
+            f"FROM {tablename} WHERE "
+            + " AND ".join([f"{col} = '{val}'" for col, val in zip(index_col, index)]),
+            **kwargs,
+        )
+    else:
+        raise Exception("Not implemented to extract multiple rows.")
+    return df.transform(
+        lambda col: col
+        if col.name not in pickled_cols
+        else col.map(lambda x: None if x is None else pickle.loads(x))
+    )
+
+
+def _quote_if_str(x: Any) -> str:
+    return f"{x}" if not isinstance(x, str) else f"'{x}'"
+
+
+def get_geschichtsseite(df_row: pd.Series) -> pd.DataFrame:
+    if not (raw_data / "metadata_api_101.db").is_file():
+        raise Exception(
+            "Error: Database not found. Initialize database using "
+            "`get_global_metadata_df(dataset, query)`."
+        )
+    with sqlite3.connect(raw_data / "metadata_api_101.db") as con:
+        tbl_exists = (
+            con.execute(
+                "SELECT 1 FROM sqlite_master WHERE name='geschichtsseiten'"
+            ).fetchone()
+            is not None
+        )
+        if (
+            not tbl_exists
+            or con.execute(
+                "SELECT 1 FROM geschichtsseiten "
+                "WHERE "
+                + " AND ".join(
+                    [
+                        f"{col} = {_quote_if_str(val)}"
+                        for col, val in zip(index_col, df_row.name)
+                    ]
+                )
+            ).fetchone()
+            is None
+        ):
+            url = _prepend_url(df_row.HIS_URL) + "?json=True"
+            _dict = requests.get(url).json()["content"]
+            [
+                _dict.pop(k)
+                for k in [
+                    *[
+                        ix.lower()
+                        for ix in df_row.index.to_list() + index_col
+                        if isinstance(ix, str)
+                    ],
+                    "breadcrumbs",
+                    "type",
+                    "sntype",
+                    "title",
+                    "nr_gp_code",
+                    "intranet",
+                    "description",
+                    "headwords",
+                    "topics",
+                    "names",
+                ]
+                if k in _dict
+            ]
+            new_row = pd.DataFrame.from_records(
+                [{k: v for k, v in _dict.items()}],
+                index=pd.MultiIndex.from_tuples((df_row.name,), names=index_col),
+            )
+            new_row[["update", "einlangen"]] = new_row[
+                ["update", "einlangen"]
+            ].transform(pd.to_datetime)
+            dtype_dict = {k: _sqlite3_type(v) for k, v in new_row.iloc[0].items()}
+            if tbl_exists:
+                db_col_set = set(_get_colnames(con, "geschichtsseiten"))
+                missing_cols = [
+                    col
+                    for col in db_col_set
+                    if col not in list(new_row.columns) + index_col
+                ]
+                new_row[missing_cols] = [None] * len(missing_cols)
+                dtype_dict.update(
+                    {k: "NUMERIC" for k in missing_cols}
+                )  # type not known
+                for col in [col for col in new_row.columns if col not in db_col_set]:
+                    if not col.replace("_", "").isalnum():
+                        raise Exception(
+                            f"Column name {col} is currently not allowed (only sepecial "
+                            "characters '_')."
+                        )
+                    con.execute(
+                        f"ALTER TABLE geschichtsseiten ADD COLUMN {col} {dtype_dict[col]}"
+                    )
+            new_row.transform(
+                lambda col: col
+                if dtype_dict[col.name] != "BLOB"
+                else col.map(pickle.dumps)
+            ).to_sql("geschichtsseiten", con, if_exists="append", dtype=dtype_dict)
+        return pd_read_sql(con, "geschichtsseiten", index=df_row.name)
 
 
 def get_global_metadata_df(
@@ -123,5 +275,8 @@ def get_antragstext(df_row: pd.Series) -> str:
         for file in path.glob("*.pdf"):
             with open(file.with_suffix(".txt"), "w") as f:
                 f.write(_extract_txt_from_pdf(file))
+        for file in path.glob("*.html"):
+            with open(file, "r") as f_in, open(file.with_suffix(".txt"), "w") as f_out:
+                f_out.write(BeautifulSoup(f_in.read(), "html.parser").body.text)
     with open(next(path.glob("*.txt")), "r") as f:
         return f.read()
