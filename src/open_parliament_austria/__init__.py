f2d491945feed7a91c6b75a7cc580f5fa55fb920
jan <152862650+j-haacker@users.noreply.github.com>
2025-10-22 00:16:27 +0200

    Remove caching db columns and index

--- src/open_parliament_austria/__init__.py
+++ src/open_parliament_austria/__init__.py
@@ -3,7 +3,6 @@
 __all__ = []
 
 from datetime import datetime
-from functools import cache
 import json
 import numpy as np
 import pandas as pd
@@ -54,7 +53,6 @@ def _extract_txt_from_pdf(pdf_file: Path | str):
     return "\n".join([p.extract_text() for p in pages])
 
 
-@cache
 def _get_rowid_index(
     con: sqlite3.Connection, tbl: str, index_col: tuple[str]
 ) -> pd.Series:
@@ -64,7 +62,6 @@ def _get_rowid_index(
     ).set_index(list(index_col))["rowid"]
 
 
-@cache
 def _get_colnames(con: sqlite3.Connection, tbl: str) -> list[str]:
     return [
         row[0]
